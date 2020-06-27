c7618cf0e0d1c0c08199bc07751aa23b38ae1eca
Lawless <vanthuan.cab93@gmail.com>
2020-06-27 13:04:29 +0700

    fix loginn

--- src/login/auth.js
+++ src/login/auth.js
@@ -4,13 +4,14 @@ var firebase = require("firebase/app");
 // Add the Firebase products that you want to use
 require("firebase/auth");
 require("firebase/firestore");
+var isValid = 2
 class Auth extends React.Component{
-
+  
   signIn(email,password){
+    
     if (email === "" || password === "") return -1 
     firebase.auth().signInWithEmailAndPassword(email,password)
     .then(function(firebaseUser) {
-      console.log("success")
       isValid = 1
 
     })
@@ -18,7 +19,6 @@ class Auth extends React.Component{
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      console.log("failed")
       isValid = 0
     });
     
@@ -30,7 +30,6 @@ class Auth extends React.Component{
       if (user) {
         var uid = user.uid;
         console.log(uid)
-        isValid = 1
         // ...
       } else {
         
@@ -38,7 +37,6 @@ class Auth extends React.Component{
     });
     return isValid
   }
-
 }
 
 export default new Auth();
